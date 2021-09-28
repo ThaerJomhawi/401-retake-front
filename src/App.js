5c4bdc1648b1618727d80207b3af98913077ead8
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-09-28 14:01:33 +0300

    u

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from "./components/Home"
-import Login from "./components/Login"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import { withAuth0 } from '@auth0/auth0-react';
-
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import FavFruit from "./components/FavFruit";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { withAuth0 } from "@auth0/auth0-react";
 
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
+    console.log("app", this.props);
     const { isAuthenticated } = this.props.auth0;
-    return(
+    return (
       <>
         <Router>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-              {this.props.auth0.isAuthenticated ? <Home/> : <Login/>}
-                {/* TODO: if the user is logged in, render the `Home` component, if they are not, render the `Login` component */}
-              </Route>
-              <Route exact path="/favFruit">
-              {this.props.auth0.isAuthenticated ? <FavFruit/> : <Login/>}
-                {/* TODO: if the user is logged in, render the `FavFruit` component, if they are not, render the `Login` component */}
-              </Route>
-            </Switch>
-            <Footer />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {this.props.auth0.isAuthenticated ? <Home /> : <Login />}
+              {/* TODO: if the user is logged in, render the `Home` component, if they are not, render the `Login` component */}
+            </Route>
+            <Route exact path="/favFruit">
+              {this.props.auth0.isAuthenticated ? <FavFruit /> : <Login />}
+              {/* TODO: if the user is logged in, render the `FavFruit` component, if they are not, render the `Login` component */}
+            </Route>
+          </Switch>
+          <Footer />
         </Router>
       </>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,68 +1,74 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { withAuth0 } from '@auth0/auth0-react';
-import {Container,Row,Col,Card,Button} from "react-bootstrap"
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { withAuth0 } from "@auth0/auth0-react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import axios from "axios"
 
 class Home extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={
-      allfruitData:[]
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      allfruitData: [],
+    };
   }
 
-  componentDidMount=()=>{
-    const{user,isAuthenticated}=this.props.auth0
-    if (isAuthenticated){
-      let apiUrl =`${process.env.REACT_APP_BACKEND_URL}/retrieve`
-      axios.get(apiUrl).then(result=>{
+  componentDidMount = () => {
+    const { user, isAuthenticated } = this.props.auth0;
+    if (isAuthenticated) {
+      let apiUrl = `${process.env.REACT_APP_BACKEND_URL}/retrieve`;
+      axios.get(apiUrl).then((result) => {
         this.setState({
-          allfruitData:result.data
-        })
-        console.log(result.data)
-      })
+          allfruitData: result.data,
+        });
+        console.log(result.data);
+      });
     }
-  }
+  };
 
-  addToFav = (index)=>{
-    let data = { name: this.state.allfruitData[index].name,
+  addToFav = (index) => {
+    let data = {
+      name: this.state.allfruitData[index].name,
       image: this.state.allfruitData[index].image,
-      price: this.state.allfruitData[index].price,}
+      price: this.state.allfruitData[index].price,
+    };
 
-      let email = this.props.auth0.user.email
-      let addUrl =`${process.env.REACT_APP_BACKEND_URL}/create/${email}`
-      axios.post(addUrl,data).then(result=>{
-        console.log(result.data)
-      })
-  }
+    let email = this.props.auth0.user.email;
+    let addUrl = `${process.env.REACT_APP_BACKEND_URL}/create/${email}`;
+    axios.post(addUrl, data).then((result) => {
+      console.log(result.data);
+    });
+  };
 
   render() {
     return (
       <>
         <h1>API Fruits</h1>
         <Container>
-        <Row>
-        {this.state.allfruitData.length > 0 && this.state.allfruitData.map(element,index)=>{
-          return(
-            <Col>
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={element.image} />
-  <Card.Body>
-    <Card.Title>{element.name}</Card.Title>
-    <Card.Text>
-      {element.price}
-    </Card.Text>
-    <Button onClick={()=>this.addToFav(index)} variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-            </Col>
-          
-          )
-        } }
-        </Row>
+          <Row>
+            {this.state.allfruitData.length > 0 &&
+              this.state.allfruitData.map((element, index) => {
+                return (
+                  <Col>
+                    <Card style={{ width: "18rem" }}>
+                      <Card.Img variant="top" src={element.image} />
+                      <Card.Body>
+                        <Card.Title>{element.name}</Card.Title>
+                        <Card.Text>{element.price}</Card.Text>
+                        <Button
+                          onClick={() => {this.addToFav(index)}}
+                          variant="primary"
+                        >
+                          add to Fav
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+          </Row>
         </Container>
       </>
-    )
+    );
   }
 }
 
